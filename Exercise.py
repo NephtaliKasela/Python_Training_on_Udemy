9cd125c8d81f87a835eff65e8b9722fef5b3803d
Nephtali Kasela <nephtalikasela04@gmail.com>
2022-07-23 18:07:00 -0700

    Add a loop that allows to ask the user to enter a password if the previous password faild.

--- Exercise.py
+++ Exercise.py
@@ -1,23 +1,25 @@
 # exercise : log in
-mdp = input("Entrez un mot de passe (min 8 caractères) : ")
+mdp = ""
 mdp_trop_court = "votre mot de passe est trop court."
 count = 0
-
-if mdp == "":
-    print(mdp_trop_court.upper())
-elif len(mdp) < 8:
-    print(mdp_trop_court.capitalize())
-else:
-    if len(mdp) >= 8:
-        flag = False
-        while count < len(mdp):
-            try:
-                mdpsecond = int(mdp[count])
-            except:
-                flag = True
-                break
-            count += 1
-        if flag == False:
-            print("Votre mot de passe ne contient que des nombres.")
-        else:
-            print("Inscription terminée.")
+flag = False
+while flag != True:
+    mdp = input("Entrez un mot de passe (min 8 caractères) : ")
+    if mdp == "":
+        print(mdp_trop_court.upper())
+    elif len(mdp) < 8:
+        print(mdp_trop_court.capitalize())
+    else:
+        if len(mdp) >= 8:
+            while count < len(mdp):
+                try:
+                    mdpsecond = int(mdp[count])
+                except:
+                    flag = True
+                    print(">> Great <<")
+                    break
+                count += 1
+            if flag == False:
+                print("Votre mot de passe ne contient que des nombres.")
+            else:
+                print("Inscription terminée.")
